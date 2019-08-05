288a8fbb5705ad955e40acd8146001d039acff77
booru-mobile <>
2019-08-05 17:15:31 -0400

    Initial commit

---
+++ precache-manifest.0e0bac330c053df7b8d4806b467040a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b2f002159d8ff14de2790f8c108c3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06474453dec550253476",
+    "url": "/static/css/main.3b06c1f0.chunk.css"
+  },
+  {
+    "revision": "01609ffaf3fc27e46c11",
+    "url": "/static/js/2.3bf63692.chunk.js"
+  },
+  {
+    "revision": "06474453dec550253476",
+    "url": "/static/js/main.8466565b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
